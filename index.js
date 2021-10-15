c755d7355ece703b92f96cc1db1e4ded63634f63
chaitanya-Uike <62866313+chaitanya-Uike@users.noreply.github.com>
2021-10-15 14:12:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,89 @@
+function createCard(note) {
+    let card = document.createElement("div");
+    card.className = "cards";
+    card.id = note.cardId;
+    card.innerHTML = `
+    <h3>${note.title}</h3>
+    <p>${note.text}</p>
+    <button id = "delete-${note.cardId}" class="delete-btn">Delete</button>`;
+    return card;
+}
+
+function checkState() {
+    let notesArray = JSON.parse(localStorage.getItem("notes"));
+
+    if (notesArray == null) {
+        notesArray = [];
+        localStorage.setItem("notes", JSON.stringify(notesArray));
+    }
+
+    let area = document.getElementById("notes-area");
+    if (notesArray.length == 0) {
+        let info = document.createElement("h4");
+        info.id = "info-card";
+        info.innerText = "Your notes will be shown here";
+        area.insertBefore(info, document.getElementById("notes-grid"));
+    }
+    else {
+        if (document.getElementById("info-card") != null)
+            area.removeChild(document.getElementById("info-card"));
+    }
+}
+
+function insertCard(card) {
+    let grid = document.getElementById("notes-grid");
+    grid.appendChild(card);
+
+    document.getElementById(`delete-${card.id}`).addEventListener("click", function (event) {
+        let ID = event.target.parentElement.id;
+
+        //remove from local storage
+        let notesArray = JSON.parse(localStorage.getItem("notes"));
+
+        let index = notesArray.findIndex(x => x.cardId == ID);
+
+        notesArray.splice(index, 1);
+
+        localStorage.setItem("notes", JSON.stringify(notesArray));
+
+        let grid = document.getElementById("notes-grid");
+        grid.removeChild(event.target.parentElement);
+
+        checkState();
+    });
+}
+
+function retrieveFromLocalStorage() {
+    let notesArray = JSON.parse(localStorage.getItem("notes"));
+    if (notesArray != null) {
+        for (let i = 0; i < notesArray.length; i++) {
+            let note = notesArray[i];
+            insertCard(createCard(note));
+        }
+    }
+}
+
+checkState();
+retrieveFromLocalStorage();
+
+document.getElementById("addNote-btn").addEventListener("click", function () {
+
+    let headingArea = document.getElementById("heading-text-area");
+    let textArea = document.getElementById("text-area");
+
+    let ID = (Date.now() + Math.random()) % 17;
+
+    let note = { cardId: ID, title: headingArea.value, text: textArea.value };
+
+    let notesArray = JSON.parse(localStorage.getItem("notes"));
+
+    notesArray.push(note);
+    localStorage.setItem("notes", JSON.stringify(notesArray));
+
+    checkState();
+
+    insertCard(createCard(note));
+
+    headingArea.value = "";
+    textArea.value = "";
+});
